4cb1c26e2417feb7c00ba9c2ed05a536a18e62e5
longlienha <longlienha20@gmail.com>
2021-05-23 23:17:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ def index() :
 
 @app.route("/tambiet")
 def tambiet() :
-    return "bye!"
+    return jsonify({"about":"longhuu"})
 
 if __name__ == "__main__":
     app.run(debug=True)
